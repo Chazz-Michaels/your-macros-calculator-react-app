bac92dca3a8a1396787316758dd34a36054cf4a6
Chazz Michaels <michaelchasedev@gmail.com>
2018-12-27 19:10:50 -0500

    remove test binding section

--- src/components/output.jsx
+++ src/components/output.jsx
@@ -114,20 +114,6 @@ class Output extends React.Component{
             carbs={gCarbs}
             fats={gFats} />
         </div>
-
-        <div id='output-wrapper' className='center-me'>
-          <div className='output-group'>Test binding here</div>
-          <div className='output-header'>Your Age: {this.props.age}</div>
-          <div className='output-header'>Your Gender: {this.props.gender}</div>
-          <div className='output-header'>Your Weight: {this.props.weight}</div>
-          <div className='output-header'>Your Height (Ft): {this.props.heightFeet}</div>
-          <div className='output-header'>Your Height (In): {this.props.heightInches}</div>
-          <div className='output-header'>Your Activity Level: {this.props.activityLevel}</div>
-          <div className='output-header'>Your goal: {this.props.goal}</div>
-          <div className='output-header'>Your Protein: {this.props.protein}</div>
-          <div className='output-header'>Your Carbs: {this.props.carbs}</div>
-          <div className='output-header'>Your Fats: {this.props.fats}</div>
-        </div>
       </React.Fragment>
     )
   }
